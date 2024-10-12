6a08f58bfc0dbd529e839302c0cbdfbf378782e2
RichardHayes <rhshadowgreen@gmail.com>
2024-10-11 21:59:24 -0700

    Godot tram partial work

--- MinnHax2024/script/tram.gd
+++ MinnHax2024/script/tram.gd
@@ -3,6 +3,11 @@ extends Node3D
 @export var tram_start_point: Node3D
 @export var tram_end_point: Node3D
 
+@export var door_left: Node3D
+@export var door_right: Node3D
+@export var door_left_open_pos: Node3D
+@export var door_right_open_pos: Node3D
+
 var gc
 
 func _ready():
@@ -12,11 +17,18 @@ func _ready():
 func place_tram_at_start():
 	set_position(tram_start_point.get_position())
 
+func tramStops():
+	gc.exitTram()
+	var tween = get_tree().create_tween()
+	tween.tween_property(door_left, "position", door_left_open_pos.get_position(), 1).set_trans(Tween.TRANS_SINE)
+	tween.set_parallel()
+	tween.tween_property(door_right, "position", door_right_open_pos.get_position(), 1).set_trans(Tween.TRANS_SINE)
+
 func start_tram():
 	gc.boardTram()
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", tram_end_point.get_position(), 10).set_trans(Tween.TRANS_SINE)
-	tween.tween_callback(gc.exitTram)
+	tween.tween_property(self, "position", tram_end_point.get_position(), 5).set_trans(Tween.TRANS_SINE)
+	tween.tween_callback(tramStops)
 
 func _process(delta):
 	pass
