650150546eb4df89834497dd82ff8dcfb0b01074
RichardHayes <rhshadowgreen@gmail.com>
2024-10-17 21:50:29 -0700

    Script updates

--- MinnHax2024/script/GameController.gd
+++ MinnHax2024/script/GameController.gd
@@ -4,10 +4,14 @@ extends Node3D
 @export var player_start_point: Node3D
 @export var player: CharacterBody3D
 
+var debug = true
+
 func _ready():
-	#tram.start_tram()
-	#player.set_position(player_start_point.get_position())
-	pass
+	if debug:
+		pass
+	else:
+		tram.start_tram()
+		player.set_position(player_start_point.get_position())
 
 func getPlayer():
 	return player
@@ -33,3 +37,25 @@ func _on_entering_gi_area_3d_body_entered(body):
 # Exiting GI
 func _on_exiting_gi_area_3d_body_entered(body):
 	print("Exiting GI")
+
+# Entering art
+func _on_entering_art_area_3d_body_entered(body):
+	print("Entering Art")
+
+func _on_exiting_art_area_3d_body_entered(body):
+	print("exiting_art")
+
+func _on_entering_arcade_area_3d_body_entered(body):
+	print("entering_arcade")
+
+func _on_exiting_arcade_area_3d_body_entered(body):
+	print("exiting_arcade")
+
+func _on_entering_jenga_area_3d_body_entered(body):
+	print("entering_jenga")
+
+func _on_exiting_jenga_area_3d_body_entered(body):
+	pass # Replace with function body.
+
+func _on_entering_beginner_area_3d_body_entered(body):
+	pass # Replace with function body.
