88e2452d7fe2449f5905b383bd3f0b8642226fdc
RichardHayes <rhshadowgreen@gmail.com>
2024-10-17 18:47:17 -0700

    Redesign heart door. Replace in Blender and then Godot

---
+++ MinnHax2024/door.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+@export var door: MeshInstance3D
+@export var barrier: StaticBody3D
+
+var openY = 9.667
+var closeY = 3.667
+
+var gc
+func _ready():
+	gc = get_tree().get_root().get_node("main")
+
+func open():
+	toggleBlocking(false)
+	var tween = create_tween()
+	var pos = door.get_position()
+	pos.y = openY
+	tween.tween_property(door, "position", pos, 0.5)
+
+func close():
+	toggleBlocking(true)
+	var tween = create_tween()
+	var pos = door.get_position()
+	pos.y = closeY
+	tween.tween_property(door, "position", pos, 0.5)
+
+var isOpen = false
+func _process(delta):
+	if Input.is_action_just_pressed("run"):
+		if isOpen:
+			close()
+			isOpen = false
+		else:
+			open()
+			isOpen = true
+
+func toggleBlocking(state):
+	if state == true:
+		barrier.remove_collision_exception_with(gc.getPlayer())
+	else:
+		barrier.add_collision_exception_with(gc.getPlayer())
--- MinnHax2024/script/GameController.gd
+++ MinnHax2024/script/GameController.gd
@@ -5,8 +5,9 @@ extends Node3D
 @export var player: CharacterBody3D
 
 func _ready():
-	tram.start_tram()
-	player.set_position(player_start_point.get_position())
+	#tram.start_tram()
+	#player.set_position(player_start_point.get_position())
+	pass
 
 func getPlayer():
 	return player
@@ -20,3 +21,15 @@ func exitTram():
 
 func _process(delta):
 	pass
+
+# Entering reception
+func _on_reception_area_3d_body_entered(body):
+	print("Entering reception")
+
+# Entering GI
+func _on_entering_gi_area_3d_body_entered(body):
+	print("Entering GI")
+
+# Exiting GI
+func _on_exiting_gi_area_3d_body_entered(body):
+	print("Exiting GI")
