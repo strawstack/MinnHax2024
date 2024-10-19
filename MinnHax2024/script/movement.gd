3794b0cf01761611a4d1f85f70a4a99b998d821e
RichardHayes <rhshadowgreen@gmail.com>
2024-10-19 14:18:24 -0700

    Working camera in terms of position and rotation

--- MinnHax2024/script/movement.gd
+++ MinnHax2024/script/movement.gd
@@ -2,12 +2,15 @@ extends CharacterBody3D
 
 var speed = 5
 var shiftSpeed = 10
+var mouse_sens = 0.3
+var camera_anglev = 0
+
+var isCameraUp = false
+var tweenCamera: Tween
+var tweenCameraRot: Tween
 
 func _ready():
 	pass
-
-var mouse_sens = 0.3
-var camera_anglev = 0
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -30,10 +33,22 @@ func get_directions():
 	}
 
 func sync_photo_camera():
-	var rotY = get_rotation().y
-	$SubViewport/photo_camera.set_rotation(Vector3(0, rotY, 0))
-	var global_pos = to_global($photo_camera_placeholder.get_position())
-	$SubViewport/photo_camera.set_position(global_pos)
+	var playerY = get_rotation().y
+	var cameraX = $Camera3D.get_rotation().x
+	$SubViewport/photo_camera.set_rotation(Vector3(cameraX, playerY, 0))
+	$SubViewport/photo_camera.set_global_position($Camera3D/camera/photo_camera_placeholder.get_global_position())
+
+func moveCamera(target, tRot):
+	if tweenCamera != null:
+		tweenCamera.pause()
+		tweenCamera.kill()
+	if tweenCameraRot != null:
+		tweenCameraRot.pause()
+		tweenCameraRot.kill()
+	tweenCamera = get_tree().create_tween()
+	tweenCameraRot = get_tree().create_tween()
+	tweenCamera.tween_property($Camera3D/camera, "position", target, 0.5)
+	tweenCameraRot.tween_property($Camera3D/camera, "rotation", tRot, 0.5)
 
 func _process(delta):
 	var dir = get_directions()
@@ -45,10 +60,21 @@ func _process(delta):
 	if Input.is_action_just_pressed("escape"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	# Player movement
 	velocity = vel.normalized() * (shiftSpeed if Input.is_action_pressed("run") else speed)
 	move_and_slide()
 	
+	# Sync position of sub viewport camera
 	sync_photo_camera()
+	
+	# Camera lift
+	if Input.is_action_just_pressed("space"):
+		if isCameraUp:
+			isCameraUp = false
+			moveCamera($camera_down.get_position() - $Camera3D.get_position(), $camera_down.get_rotation())
+		else:
+			isCameraUp = true
+			moveCamera($camera_up.get_position() - $Camera3D.get_position(), $camera_up.get_rotation())
 
 var push_force = 10.0
 func _physics_process(delta):
