11269e4b7a5fd2ffba2519c3d379aeabb1b7bfd6
RichardHayes <rhshadowgreen@gmail.com>
2024-10-20 18:18:20 -0700

    orby light cone and light. Orby looks at player in reception area

--- MinnHax2024/script/spark.gd
+++ MinnHax2024/script/spark.gd
@@ -6,6 +6,8 @@ var oneSecPer = 2.0
 var isTrackingPlayer = false
 var player
 
+var SMOOTH_SPEED = 10
+
 var gc
 func _ready():
 	gc = get_tree().get_root().get_node("main")
@@ -35,4 +37,9 @@ func trackPlayer(state):
 
 func _process(delta):
 	if isTrackingPlayer:
-		look_at(player.get_position())
+		var op = get_position() # Orby position
+		var pp = player.get_position() # Player pos
+		var d = (Vector2(pp.x, pp.z) - Vector2(op.x, op.z)).length() # Horizontal dist orby to player
+		var h = pp.y - op.y # Vertical distance
+		$body.set_rotation(Vector3(lerp_angle($body.rotation.x, atan2(h, d), delta * SMOOTH_SPEED), 0, 0))
+		set_rotation(Vector3(0, lerp_angle(rotation.y, atan2(op.x - pp.x, op.z - pp.z), delta * SMOOTH_SPEED), 0))
